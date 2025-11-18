7fe3d28bf6ec8908785499111ba1342cf146a026
Varun Trikha <143183439+student-Varun@users.noreply.github.com>
2025-11-19 01:00:10 +0530

    Update ALLOWED_HOSTS for production settings

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'y!^f73w5xi5zi17+q&hu+krknlm*j&018xfy8xx5(a)axn0%b%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost']
+
 
 
 # Application definition
